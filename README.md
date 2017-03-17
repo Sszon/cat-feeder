410dda7a774b809501d058a171dd8c1877d01057
Sszon <patrik.larses@gmail.com>
2017-03-17 14:59:14 +0100

    initial version of elsie test, is she hungry?

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # cat-feeder
 A feeder that feeds the cat
+
+How does it work?
+- The cat need to be able to be fed, 3 times per day
+-- These times needs to be able to be changed
+- OR the cat gets fed by pushing a a button
+- OR pushing a button on the web-page
+- The cat gets fed by a turning motor. It turn a certain amount of degrees 
+
+How can i make it easier/simpler?
---
+++ elsie.py
@@ -0,0 +1,32 @@
+def test_Elsie():
+    #How is Elsie?
+    #Is she hungry
+    print hungry()
+    #Is she starving
+    print starving()
+    #Has she been fed?
+    print fed()
+
+def hungry():
+    #Elsie is always hungry
+    return "Elsie is hungry"
+
+def fed():
+    #Has she been fed?
+    return "You have not fed me!"
+
+def starving(n):
+    #is Elsie starving?
+    
+    #That means she havent 
+    #been fed on the set time that she was supposed to be fed
+    return "Elsie is starving"
+
+def feed():
+    #Feed her
+    latest_feed_time = get_time
+
+
+    
+
+print test_Elsie()
